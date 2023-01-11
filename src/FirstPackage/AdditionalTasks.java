8fb8e0167131f47dfd71b132f6ec1565aa4b480a
azamatbazik <bazarbai22.09@gmail.com>
2023-01-11 11:23:08 +0600

    add new task is leetcode

--- src/FirstPackage/AdditionalTasks.java
+++ src/FirstPackage/AdditionalTasks.java
@@ -2,7 +2,6 @@ package FirstPackage;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.*;
 
 public class AdditionalTasks implements Test {
@@ -138,46 +137,50 @@ public class AdditionalTasks implements Test {
         return arr.length % 2 == 0 ? (double) (arr[i] + arr[i - 1]) / 2 : (double) arr[i];
 
     }
+
     public int reverse(int x) {
         int copyValue = x;
         double reverse = 0;
-        while(copyValue!=0){
-            int remainder = copyValue%10;
-            reverse = reverse*10+remainder;
-            copyValue=copyValue/10;
+        while (copyValue != 0) {
+            int remainder = copyValue % 10;
+            reverse = reverse * 10 + remainder;
+            copyValue = copyValue / 10;
         }
-        if(reverse> Integer.MAX_VALUE){
-            reverse=0;
-        }else if(reverse<Integer.MIN_VALUE){
-            reverse=0;
+        if (reverse > Integer.MAX_VALUE) {
+            reverse = 0;
+        } else if (reverse < Integer.MIN_VALUE) {
+            reverse = 0;
         }
-        return (int)reverse;
+        return (int) reverse;
     }
 
     public int divide(int dividend, int divisor) {
-        return (dividend)/(divisor);
+        return (dividend) / (divisor);
     }
+
     public String toLowerCase(String s) {
         return s.toLowerCase();
     }
+
     public int commonFactors(int a, int b) {
         int minNumber = 0;
         int result = 0;
-        if(a>b){
-            minNumber=b;
-        }else {
-            minNumber=a;
+        if (a > b) {
+            minNumber = b;
+        } else {
+            minNumber = a;
         }
         for (int i = 1; i <= minNumber; i++) {
-            if(a%i==0 && b%i==0){
+            if (a % i == 0 && b % i == 0) {
                 result++;
             }
         }
         return result;
     }
+
     public int maxProduct(int[] nums) {
         Arrays.sort(nums);
-        return (nums[nums.length-1]-1)*(nums[nums.length-2]-1);
+        return (nums[nums.length - 1] - 1) * (nums[nums.length - 2] - 1);
     }
 
     public String firstPalindrome(String[] words) {
@@ -185,11 +188,11 @@ public class AdditionalTasks implements Test {
         for (int i = 0; i < words.length; i++) {
             char[] copyValue = words[i].toCharArray();
             char[] secondCopyValue = new char[copyValue.length];
-            int copyLength = copyValue.length-1;
+            int copyLength = copyValue.length - 1;
             for (int j = 0; j < secondCopyValue.length; j++, copyLength--) {
                 secondCopyValue[j] = copyValue[copyLength];
             }
-            if(String.valueOf(copyValue).equals(String.valueOf(secondCopyValue))){
+            if (String.valueOf(copyValue).equals(String.valueOf(secondCopyValue))) {
                 result = words[i];
                 break;
             }
@@ -201,10 +204,10 @@ public class AdditionalTasks implements Test {
         int index = word.indexOf(ch);
         char[] copyValue = new char[word.length()];
         for (int i = 0; i < copyValue.length; i++) {
-            if(index>=0){
+            if (index >= 0) {
                 copyValue[i] = word.charAt(index);
                 index--;
-            }else{
+            } else {
                 copyValue[i] = word.charAt(i);
             }
         }
@@ -212,31 +215,28 @@ public class AdditionalTasks implements Test {
     }
 
     public boolean halvesAreAlike(String s) {
-        int l=s.length();
-        int count1=0;
-        int count2=0;
+        int l = s.length();
+        int count1 = 0;
+        int count2 = 0;
 
-        for(int i=0;i<l/2;i++)
-        {
-            if( checkStr(s.charAt(i)))
+        for (int i = 0; i < l / 2; i++) {
+            if (checkStr(s.charAt(i)))
                 count1++;
         }
 
-        for(int i=l/2;i<l;i++)
-        {
-            if( checkStr(s.charAt(i)))
+        for (int i = l / 2; i < l; i++) {
+            if (checkStr(s.charAt(i)))
                 count2++;
         }
-        if(count1==count2)
+        if (count1 == count2)
             return true;
 
         return false;
 
     }
 
-    boolean checkStr(char ch)
-    {
-        if(ch=='a' || ch=='A' || ch=='e'|| ch=='E'||ch=='i'|| ch=='I'|| ch=='o'|| ch=='O'|| ch=='u'|| ch=='U')
+    boolean checkStr(char ch) {
+        if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U')
             return true;
 
         return false;
@@ -245,39 +245,159 @@ public class AdditionalTasks implements Test {
     public int prefixCount(String[] words, String pref) {
         int result = 0;
         for (int i = 0; i < words.length; i++) {
-            if(words[i].startsWith(pref)){
+            if (words[i].startsWith(pref)) {
                 result++;
             }
         }
         return result;
     }
+
     public int findNumbers(int[] nums) {
         int result = 0;
         for (int i = 0; i < nums.length; i++) {
             String valueLength = String.valueOf(nums[i]);
-            if(valueLength.length()%2==0){
+            if (valueLength.length() % 2 == 0) {
                 result++;
             }
         }
         return result;
     }
-    public boolean areOccurrencesEqual(String s)
-    {
+
+    public boolean areOccurrencesEqual(String s) {
         int[] freq = new int[26];
         int max = 0;
-        for(char ch : s.toCharArray())
-        {
-            int idxFromChar = ch-'a';
+        for (char ch : s.toCharArray()) {
+            int idxFromChar = ch - 'a';
             max = Math.max(max, ++freq[idxFromChar]);
         }
 
-        for(int freqValue : freq)
-        {
-            if(freqValue != 0 && freqValue != max)
+        for (int freqValue : freq) {
+            if (freqValue != 0 && freqValue != max)
                 return false;
         }
         return true;
 
+    }
+
+    public boolean isStrictlyPalindromic(int n) {
+        boolean result = false;
+        char[] binaryValue = (Integer.toBinaryString(n)).toCharArray();
+        char[] copyBinaryValue = new char[binaryValue.length];
+        int a = 0;
+        for (int i = copyBinaryValue.length - 1; i > 0; i--) {
+            copyBinaryValue[a] = binaryValue[i];
+            a++;
+        }
+        if (String.valueOf(binaryValue).equals(String.valueOf(copyBinaryValue))) {
+            result = true;
+        }
+        return result;
+    }
+
+    public int Liters(double time) {
+
+        //Your code goes here! Hint: You should change that -1
+        int result = (int) time / 2;
+        return result;
+    }
+
+    public static int Past(int h, int m, int s) {
+        //Happy Coding! ^_^
+        int result = 0;
+        if (h == 0) {
+            result += 0;
+        } else {
+            result += h * (60 * 60 * 1000);
+        }
+        {
+            if (m == 0) {
+                result += 0;
+            } else {
+                result += m * (60 * 1000);
+            }
+        }
+        if (s == 0) {
+            result += 0;
+        } else {
+            result += s * 1000;
+        }
+        return result;
+    }
+
+    public static int[] invert(int[] array) {
+        int[] result = new int[array.length];
+        for (int i = 0; i < result.length; i++) {
+            if (array[i] > 0) {
+                result[i] = (array[i] - (array[i] + array[i]));
+            } else {
+                result[i] = ((array[i] + array[i]));
+            }
+        }
+
+        return result;
+    }
+
+
+    public int minSteps(String s, String t) {
+
+        int[] res = validAnagram(s);
+        int[] res1 = validAnagram(t);
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if (res1[i] < res[i]) {
+                ans += res[i] - res1[i];
+            }
+        }
+        return ans;
+    }
+
+    static int[] validAnagram(String s) {
+        int[] ans = new int[26];
+        for (char c : s.toCharArray()) {
+            ans[c - 'a']++;
+        }
+        return ans;
+    }
+
+    public int minValueInArray(int[] array) {
+        if (array.length == 0) return 0;
+        int minValue = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+        }
+        return minValue;
+    }
+
+    public int[] sortArrayBubble(int[] array) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    isSorted = false;
+                }
+            }
+        }
+        return array;
+    }
+
+    public int secondBigValue(int[] array) {
+        int maxi1 = Integer.MIN_VALUE;
+        int maxi2 = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (maxi1 < array[i]) {
+                maxi2 = maxi1;
+                maxi1 = array[i];
+            } else if (maxi2 < array[i]) {
+                maxi2 = array[i];
+            }
+        }
+        return maxi2;
     }
 
     public int dataHashcode(File value) {
--- src/FirstPackage/Main.java
+++ src/FirstPackage/Main.java
@@ -1,15 +1,17 @@
 package FirstPackage;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
         AdditionalTasks additionalTasks = new AdditionalTasks();
-        String[] value = new String[]{"pay","attention","practice","attend"};
-        String firstValue = "at";
-        System.out.println(additionalTasks.areOccurrencesEqual("aaabb"));
+        int[] array = new int[]{-5, -1,-2,-4};
+        System.out.println(additionalTasks.secondBigValue(array));
     }
 }
